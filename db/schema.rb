7472b79f9661698e1222baef8361f6623b4cec48
TimBlanchard <timothee.blanchard37@gmail.com>
2018-06-08 12:45:04 +0200

    commit initial

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180603144034) do
+ActiveRecord::Schema.define(version: 20180608103637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20180603144034) do
     t.string "title"
     t.text "content"
     t.integer "stars"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
